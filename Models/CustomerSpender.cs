71ee5f2bf81924b8cd3cf94e04e2d1ebd107aa84
Pavel Shwan Ibrahim <pavel.shwan.ibrahim@no.experis.com>
2023-02-22 16:44:56 +0100

    Added new variable for CustomerSpender class

--- Models/CustomerSpender.cs
+++ Models/CustomerSpender.cs
@@ -6,5 +6,8 @@ using System.Threading.Tasks;
 
 namespace SuperheroesDb_Project.Models
 {
-    public class CustomerSpender{}
+    public class CustomerSpender
+    { // We only include Total because that's the only thing needed in the assignment (Same as certain values from Customer)
+        public int Total { get; set; }
+    }
 }
